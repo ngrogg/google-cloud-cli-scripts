82a7e2d31625ce5cf7a0cb356adfae7bd9d0dbd1
Nicholas Grogg <Nicholas_Grogg6505@outlook.com>
2025-08-20 21:41:53 +0000

    Added scripts/listScripts/diskFind script. scripts/listScripts/README.md Updated for new diskFind script.

--- scripts/listScripts/README.md
+++ scripts/listScripts/README.md
@@ -5,6 +5,9 @@ BASH scripts for listing servers matching criteria in GCP. Useful for upgrade pr
 
 ## Scripts
 * **centosList**, A BASH script for listing CentOS 7 servers in GCP.
+* diskFind, A BASH script for finding disk types across projects. <br>
+  Usage. `./diskFind.sh DISK_TYPE` <br>
+  Ex. `./diskFind.sh pd-standard` <br>
 * **databaseTechList**, A BASH script for listing MariaDB, MySQL and Postgres daugs and whatever version of the tech they're running. Takes a database tech and version as arguments. <br>
   Usage. `./databaseTechList.sh list DATABASE_TECH TECH_VERSION` <br>
   Ex. `./databaseTechList.sh list mysql 8` <br>
---
+++ scripts/listScripts/diskFind/diskFind.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/bash
+
+# Disk Find
+# BASH script to find all instances of a disk type across projects
+# By Nicholas Grogg
+
+if [[ $1 == "help" || $1 == "Help" ]]; then
+        echo "Find disks across projects that match a criteria type"
+        echo "Usage. ./diskFind.sh DISK_TYPE"
+        echo "Ex. ./diskFind.sh pd-standard"
+fi
+
+if [[ -z $1 ]]; then
+        echo "Argument not provided, exiting!"
+        echo "Usage. ./diskFind.sh DISK_TYPE"
+        echo "Ex. ./diskFind.sh pd-standard"
+        exit 1
+fi
+
+# Assign provided diskType value to variable
+diskType=$1
+
+# Generate array of projects, adjust regex as needed
+projectArray=(`gcloud projects list | grep -E -v 'PROJECT_ID|^development|^test' | awk '{print $1}'`)
+
+# Create folders as needed if they don't exist
+if [[ ! -d interimFiles ]]; then
+        mkdir interimFiles
+fi
+
+if [[ ! -d outputFiles ]]; then
+        mkdir outputFiles
+fi
+
+# Create final output csv file for script
+echo "name,size,project" > diskFindOutput.csv
+
+# Iterate through array of projects looking for disks that match the provided disk type
+for project in "${projectArray[@]}"; do
+    ## Append disks to csv, remove first line of output
+    gcloud compute disks list --project="${project}" --filter="type:$diskType" --format='csv(name,sizeGb)' | tail -n +2 > interimFiles/${project}.csv
+
+    ## Append project to each line of csv file if csv file has any words at all in it
+    if [[ $(wc -w interimFiles/${project}.csv | awk '{print $1}') -gt 0 ]]; then
+            sed -i "s/$/,${project}/" interimFiles/${project}.csv
+    fi
+done
+
+# Combine interim files into one master CSV file, only add files if not empty
+for file in $(ls interimFiles/); do
+    if [[ $(wc -w interimFiles/$file | awk '{print $1}') -gt 0 ]]; then
+        cat interimFiles/$file >> diskFindOutput.csv
+    fi
+done
+
+# Back up master csv w/ timestamp
+cp diskFindOutput.csv outputFiles/diskFindOutput.$(date +%Y%m%d%H%M).csv
+
